46887629224043e19ce5d0ab4ce3b6556d9f0494
Abdul Hannan <hannan7897a@gmail.com>
2026-02-16 23:51:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -102,3 +102,4 @@ elif st.session_state.page == 'seo':
     if st.button("Analyze"):
         st.area_chart([20, 50, 40, 100, 80])
     st.button("⬅️ Back", on_click=lambda: navigate('dashboard'))
+
